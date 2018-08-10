719b5d38860de5d21c94016bae4ed9514856ef02
tae-lim <taelim92@gmail.com>
2018-08-10 15:53:29 -0700

    Implemented user input map to escaped string

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,38 @@
 // YOUR CODE HERE:
+
+
+let escape = (submission) => {
+  //get input from user
+  let input = submission.split('');
+  let escapees = {
+    '&' : '&amp;',
+    '<' : '&lt;',
+    '>' : '&gt;',
+    '\"' : '&quot;',
+    '\'' : '&#x27;',
+    '/' : '&#x2F;'
+  };
+  //loop through each character
+  for (let i = 0; i < input.length; i++) {
+    for (let key in escapees) {
+      if (input[i] === key) {
+        input[i] = escapees[key];
+      }
+    }
+  }
+  return input.join('');
+    //if it is < > & , etc
+      //replace with alt;, amp;, etc
+    //return the modified string
+};
+
+// & --> &amp;
+//  < --> &lt;
+//  > --> &gt;
+//  " --> &quot;
+//   ' --> &#x27;     &apos; not recommended because its not in the HTML spec (See: section 24.4.1) &apos; is in the XML and XHTML specs.
+//  / --> &#x2F;     forward slash is included as it helps end an HTML entity
+
+
+//input: user submission (username, message, friend's name during search, naming a room)
+//output: modified string
